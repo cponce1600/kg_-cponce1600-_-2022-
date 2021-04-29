f03db7d8910721f618223e67edaf975b4233cb5d
cponce1600 <56129964+cponce1600@users.noreply.github.com>
2021-04-29 12:32:54 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-/*
-*This version of the solution is non functional
-*It lays the ground work for my soultion. 
-*/
-
 //This object represents the kkey value pairs that will be used 
 //to compare digits to their phonetic equvlet. 
 var digToLit = {"1": "One", "2": "Two", 
@@ -10,22 +5,30 @@ var digToLit = {"1": "One", "2": "Two",
                       "5": "Five",  "6": "Six",
                       "7": "Seven", "8": "Eight", 
                       "9": "Nine",  "0": "Zero"};
-//this vaiebles will be used are reused in the getPhonetic function
+//These vaiebles will be used are reused in the getPhonetic function
 var sIntArr = new Array();
 var sInt;
 
 //This function will take an int array as its parameters and print
 //the phonetic equivelent 
-function getPhonetic(intArr){
-  //here we will use a for loop to vist all elements of the intArr 
-  //these elements will be sent to a helper method called getDigits
-  //the restuls of getDigits will be saved in sIntArr
-  //the function will then print our all the elemetns in sIntArr
+function getPhonetic(intArr){ 
+  for(num in intArr){
+    sInt = getDigits(intArr[num].toString());
+    sIntArr.push(sInt);
+  }
+  
+  console.log(sIntArr);
+  sIntArr.length = 0;
 }
 
 //This function is only used by getPhonetic
-//It will take a string as it's paramter and return a string
-//Its main task is to get the phonetic equivlent of the digit
+//it takes in a string and recursivly gets the phonetic equivlent 
 function getDigits(sInt){
-
+  if(sInt == "")
+    return "";
+  else
+    return digToLit[sInt.charAt(0)] + getDigits(sInt.substring(1));
+  
 }
+
+getPhonetic([12123,54]);
